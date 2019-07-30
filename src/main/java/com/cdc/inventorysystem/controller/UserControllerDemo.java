2642de90fbb90c66d3aa7d7302e351d279187298
PC201803230922\Administrator <1978917933@qq. com>
2019-07-30 16:41:56 +0800

    Revert "测试"
    This reverts commit 0498db2c636bc1aeae5e381e796658ab7cb9588c.

--- src/main/java/com/cdc/inventorysystem/controller/UserControllerDemo.java
+++ src/main/java/com/cdc/inventorysystem/controller/UserControllerDemo.java
@@ -45,10 +45,6 @@ public class UserControllerDemo {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public ResponseVO test() {
-    	
-    	
-    	
-    	
         userService.test();
         return new ResponseVO(ResponseStatusEnum.SUCCESS, "");
     }
