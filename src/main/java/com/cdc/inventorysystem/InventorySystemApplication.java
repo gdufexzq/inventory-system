4c9ff3a68d17f8c1624ef0bd483cb2a4f1755d1b
xzquan <xzquan@cndatacom.com>
2019-08-02 14:41:17 +0800

    提交到xzquan

--- src/main/java/com/cdc/inventorysystem/InventorySystemApplication.java
+++ src/main/java/com/cdc/inventorysystem/InventorySystemApplication.java
@@ -3,7 +3,7 @@ package com.cdc.inventorysystem;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-@MapperScan("{com.cdc.inventorysystem.dao,com.cdc.inventorysystem.mapper}")
+@MapperScan("com.cdc.inventorysystem.dao")
 @SpringBootApplication
 public class InventorySystemApplication {
 
