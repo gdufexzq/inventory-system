197ff5dbc012dd9d552c2aeb41d16159ede70ed7
xzquan <xzquan@cndatacom.com>
2019-08-06 11:06:33 +0800

    更新develop

--- src/main/java/com/cdc/inventorysystem/entity/Mission.java
+++ src/main/java/com/cdc/inventorysystem/entity/Mission.java
@@ -7,9 +7,14 @@ import java.util.Date;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 
 /**
  * <p>
@@ -21,68 +26,81 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
+@Document(indexName = "inventory-system-index", type = "mission", shards = 1, replicas = 0, refreshInterval = "-1")
 public class Mission implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
     @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     /**
      * 任务标题
      */
+    @Field
     private String title;
 
     /**
      * 任务概述
      */
+    @Field
     private String content;
 
     /**
      * 任务积分
      */
+    @Field
     private Integer score;
 
     /**
      * 发布人
      */
+    @Field
     @TableField("userId")
     private Integer userId;
 
     /**
      * 发布时间
      */
+    @Field
     @TableField("pubTime")
-    private String pubTime;
+//    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date pubTime;
 
     /**
      * 发布地点ID
      */
+    @Field
     @TableField("schoolId")
     private Integer schoolId;
 
     /**
      * 接受者ID
      */
+    @Field
     @TableField("recUserId")
     private Integer recUserId;
 
     /**
      * 接受时间
      */
+    @Field
     @TableField("recTime")
     private String recTime;
 
     /**
      * 状态 0:待接受 1:已接受 2:已完成
      */
+    @Field
     private Integer state;
 
     /**
      * 是否屏蔽 0:否 1:是  缺省值为0
      */
+    @Field
     private Integer display;
-	private String schoolName;
-	private String recUser;
+//	private String schoolName;
+//	private String recUser;
 
 	public Mission(Integer userId) {
 		super();
---
+++ src/main/java/com/cdc/inventorysystem/service/impl/MissionESServiceImpl.java
@@ -0,0 +1,153 @@
+package com.cdc.inventorysystem.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import com.cdc.inventorysystem.dao.MissionMapper;
+import com.cdc.inventorysystem.entity.Mission;
+import com.cdc.inventorysystem.entity.vo.ResponseVO;
+import com.cdc.inventorysystem.repository.MissionRepository;
+import com.cdc.inventorysystem.service.MissionESService;
+import com.cdc.inventorysystem.service.MissionService;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author xuzhiquan
+ * @since 2019-08-01
+ */
+@Service
+public class MissionESServiceImpl implements MissionESService {
+	@Autowired
+	private MissionRepository missionRepository;
+	@Autowired
+	private ElasticsearchTemplate elasticsearchTemplate;
+	@Autowired
+	private MissionService missionService;
+
+	// 根据id删除记录
+	@Override
+	public void deleteDocumentById(int id) {
+		missionRepository.deleteById(1);
+	}
+
+
+	@Override
+	public List<Mission> findAll() {
+		Iterable<Mission> missions = missionRepository.findAll();
+		List<Mission> missionsList = new ArrayList<>();
+		ArrayList<Integer> idList = new ArrayList<>();
+		System.out.println(missions);
+		missions.forEach(mission -> {
+			System.out.println(mission);
+			Integer id = mission.getId();
+			idList.add(id);
+		});
+
+		QueryWrapper queryWrapper = new QueryWrapper<>();
+		queryWrapper.in("Id", idList);
+		List<Mission> list = missionService.list(queryWrapper);
+		System.out.println(list);
+		return list;
+	}
+
+
+	@RequestMapping("/findById")
+	public Mission findById() throws Exception {
+		Optional<Mission> optional = missionRepository.findById(1);
+		Mission mission = optional.get();
+		System.out.println(mission);
+		return mission;
+	}
+
+
+	@RequestMapping("/findByTitle")
+	public List<Mission> findByTitle() throws Exception {
+		List<Mission> list = missionRepository.findByTitle("测试工具");
+		list.forEach(mission -> System.out.println(mission));
+		return list;
+	}
+
+	public void testNativeSearchQuery() throws Exception {
+		// 创建一个查询对象
+//        maven是一个工程构建工具
+		NativeSearchQuery query = new NativeSearchQueryBuilder()
+				.withQuery(QueryBuilders.queryStringQuery("是").defaultField("title"))
+				.withPageable(PageRequest.of(0, 15))
+				.withHighlightFields(new HighlightBuilder.Field("title"))
+				.build();
+		// 执行查询
+		AggregatedPage<Mission> missions = elasticsearchTemplate.queryForPage(query, Mission.class,
+				new SearchResultMapper() {
+					@Override
+					public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+						ArrayList<Mission> missions = new ArrayList<>();
+						SearchHits hits = response.getHits();
+						for (SearchHit hit:hits){
+							if(hits.getHits().length <= 0) {
+								return null;
+							}
+							Mission mission = new Mission();
+							String highLightMessage = hit.getHighlightFields().get("title").fragments()[0].toString();
+							mission.setId(Integer.parseInt(hit.getId()));
+							mission.setTitle(hit.getSourceAsMap().get("title").toString());
+							mission.setContent(hit.getSourceAsMap().get("content").toString());
+							try {
+								String setMethodName = parSetName("title");
+								Class<? extends Mission> missionClass = Mission.class;
+								Method setMethod = missionClass.getMethod(setMethodName, String.class);
+								setMethod.invoke(mission, highLightMessage);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							missions.add(mission);
+						}
+						if(missions.size() > 0) {
+							return new AggregatedPageImpl<>((List<T>) missions);
+						}
+						return null;
+					}
+				});
+		if(missions != null && missions.getSize() > 0) {
+			missions.getContent().forEach(mission -> System.out.println(mission));
+		}
+	}
+
+	public String parSetName(String fieldName){
+		if (null == fieldName || "".equals(fieldName)) {
+			return null;
+		}
+		int startIndex = 0;
+		if (fieldName.charAt(0) == '_') {
+			startIndex = 1;
+		}
+		return "set" + fieldName.substring(startIndex, startIndex + 1).toUpperCase()
+				+ fieldName.substring(startIndex + 1);
+	}
+
+}
